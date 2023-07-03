c219ccf2c6cce49c1b93c3e11e320a1790bdb9df
Alexandr1902 <aleksandr.dev1902@gmail.com>
2023-07-03 15:55:40 +0300

    add old api

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const fetchProductData = () => {
-  return axios.get(`${}/products`)
+  return axios.get(`${REACT_APP_API}/products`)
       .then((res) => res.data)
       .catch(e => console.error('Ошибка при получении списка продуктов: ' ,e));
 }
