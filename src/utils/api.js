ea02afa187d6b74408696099d2dc580a341bcf7a
Alexandr1902 <aleksandr.dev1902@gmail.com>
2023-07-03 15:29:44 +0300

    change fetch url

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const fetchProductData = () => {
-  return axios.get(`${process.env.REACT_APP_API}/products`)
+  return axios.get(`https://ugmk-test-server.vercel.app/products`)
       .then((res) => res.data)
       .catch(e => console.error('Ошибка при получении списка продуктов: ' ,e));
 }
